dcb9bba7933c1f8b755c8396639fd3963e9000d4
Keith Johnson <kjohnson0451@gmail.com>
2017-03-10 13:43:02 -0500

    Manually defined each route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles do
-    resources :comments
-  end
+  # Options to play around with
+  # get 'articles', to: 'articles#index', as: 'something', path: 'blahblah'
+
+  # ARTICLES RESOURCES
+  get 'articles', to: 'articles#index'
+  post '', to: 'articles#create', path: 'articles'
+  get 'new_article', to: 'articles#new', path: 'articles/new'
+  get 'edit_article', to: 'articles#edit', path: 'articles/:id/edit'
+  get 'article', to: 'articles#show', path: 'articles/:id'
+  patch '', to: 'articles#update', path: 'articles/:id'
+  put '', to:'articles#update', path: 'articles/:id'
+  delete '', to:'articles#destroy', path: 'articles/:id'
+
+  # COMMENTS RESOURCES
+  get 'article_comments', to: 'comments#index', path: 'articles/:article_id/comments'
+  post '', to: 'comments#create', path: 'articles/:article_id/comments'
+  get 'new_article_comment', to: 'comments#new', path: 'articles/:article_id/comments/new'
+  get 'edit_article_comment', to: 'comments#edit', path: '/articles/:article_id/comments/:id/edit'
+  get 'article_comment', to: 'comments#show', path: '/articles/:article_id/comments/:id'
+  patch '', to: 'comments#update', path: '/articles/:article_id/comments/:id'
+  put '', to: 'comments#update', path: '/articles/:article_id/comments/:id'
+  delete '', to: 'comments#destroy', path: '/articles/:article_id/comments/:id'
 
   root 'welcome#index'
 end
